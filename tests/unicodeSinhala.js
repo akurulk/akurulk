21d100968fef61811b3b65530d0097343770085f
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2012-01-21 12:36:56 +0530

    added Unicode Sinhala Alphabat Detector

--- tests/unicodeSinhala.js
+++ tests/unicodeSinhala.js
@@ -1,3 +1,71 @@
-test('sample test', function() {
-	ok(true);
+test('getType() consonents', function() {
+
+	var us = new UnicodeSinhala();
+	equal(us.getType('ක'), UnicodeSinhala.CONSONANT);
+	equal(us.getType('ච'), UnicodeSinhala.CONSONANT);
+	equal(us.getType('ෆ'), UnicodeSinhala.CONSONANT);
+
+});
+
+test('getType() not a consonent', function() {
+
+	var us = new UnicodeSinhala();
+	notEqual(us.getType('ා'), UnicodeSinhala.CONSONANT);
+});
+
+test('getType() dependant vowel', function() {
+
+	var us = new UnicodeSinhala();
+	equal(us.getType('්'), UnicodeSinhala.DEPENDENT_VOWEL);
+	equal(us.getType('ා'), UnicodeSinhala.DEPENDENT_VOWEL);
+	equal(us.getType('ෳ'), UnicodeSinhala.DEPENDENT_VOWEL);
+
+});
+
+test('getType() dependant vowel', function() {
+
+	var us = new UnicodeSinhala();
+	notEqual(us.getType('ෆ'), UnicodeSinhala.DEPENDENT_VOWEL);
+});
+
+test('getType() independant vowel', function() {
+
+	var us = new UnicodeSinhala();
+	equal(us.getType('අ'), UnicodeSinhala.INDEPENDENT_VOWEL);
+	equal(us.getType('ඕ'), UnicodeSinhala.INDEPENDENT_VOWEL);
+	equal(us.getType('ඖ'), UnicodeSinhala.INDEPENDENT_VOWEL);
+
+});
+
+test('getType() independant vowel', function() {
+
+	var us = new UnicodeSinhala();
+	notEqual(us.getType('ෆ'), UnicodeSinhala.INDEPENDENT_VOWEL);
+});
+
+test('getType() sign', function() {
+
+	var us = new UnicodeSinhala();
+	equal(us.getType('ං'), UnicodeSinhala.SIGN);
+	equal(us.getType('ඃ'), UnicodeSinhala.SIGN);
+
+});
+
+test('getType() not a sign', function() {
+
+	var us = new UnicodeSinhala();
+	notEqual(us.getType('ා'), UnicodeSinhala.SIGN);
+});
+
+test('getType() punctuation', function() {
+
+	var us = new UnicodeSinhala();
+	equal(us.getType('෴'), UnicodeSinhala.PUNCTUATION);
+
+});
+
+test('getType() not a punctuation', function() {
+
+	var us = new UnicodeSinhala();
+	notEqual(us.getType('ා'), UnicodeSinhala.PUNCTUATION);
 });
