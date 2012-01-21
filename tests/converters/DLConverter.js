931bbf3e49fb38d8fc1342baf3785a586ad9ebd7
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2012-01-21 18:36:26 +0530

    DLConverter added

---
+++ tests/converters/DLConverter.js
@@ -0,0 +1,12 @@
+test('basic convert', function() {
+	
+	var dl = new DLConverter();
+	equal(dl.convert('අරුනෝද සුසිරිපාල'), 'wrefkdao iqisßmd,');
+});
+
+test('symboles basic', function() {
+	
+	var dl = new DLConverter();
+	equal(dl.convert('()+\'/?:'), '^&¤~$//(_');
+	equal(dl.convert('!-'), '`)');
+})
