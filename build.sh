a4cfad7d0ec7e933119f17cf79bb44b544bfbd91
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2012-01-22 11:29:18 +0530

    added distribution loading zepto if jQuery is not available

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Bash Script for Building AkuruLK JS Client
+# Uses uglify-js 
+#  install it via `sudo npm install uglify-js -g`
+
+
+#Clean and create the Dist Directory
+rm dist -rf && mkdir dist
+
+#Minify and Mix
+echo "Minifying Started"
+
+uglifyjs lib/prefix.js >> dist/akuru.min.js
+uglifyjs lib/unicodeSinhala.js >> dist/akuru.min.js
+uglifyjs lib/tokenizer.js >> dist/akuru.min.js
+uglifyjs lib/converters/DLConverter.js >> dist/akuru.min.js
+uglifyjs lib/vendors/htmlparser.js >> dist/akuru.min.js
+uglifyjs lib/akuru.js >> dist/akuru.min.js
+uglifyjs lib/loader.js >> dist/akuru.min.js
+
+echo "Conpying additional files to dist"
+
+cp lib/fonts dist -r
+cp lib/vendors dist -r
+
+
+		
